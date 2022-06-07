b3a58f093aea466610c01b91589e838fae5f0a7e
Developer-Fan <81670490+Developer-Fan@users.noreply.github.com>
2022-06-08 09:38:41 +1000

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-import "./styles.css";
+//Setup canvas
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+//Sprite setup
+class sprite {
+  constructor(x, y, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.h = h;
+    this.w = w;
+    this.color = color;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+  }
+  update() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+}
+
+//Setup gameboard
+var sprites = [];
+
+var game = {
+  clear: function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  },
+  update: function () {
+    this.clear();
+    for (var i = 0; i < sprites.length; i++) {
+      sprites[i].update();
+    }
+  }
+};
+
+sprites.push(new sprite(0, 0, 20, 40, "red"));
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    sprites[0].x += 10;
+    game.update();
+  } else if (e.key === "ArrowLeft") {
+    sprites[0].x -= 10;
+    game.update();
+  }
+});
